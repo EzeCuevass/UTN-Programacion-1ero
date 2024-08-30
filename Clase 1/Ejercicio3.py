8a77e2e09567d23f835e4ab95d5fdc3c9a8e5133
gh0s7face <eze.cuevass@gmail.com>
2024-08-30 17:53:30 -0300

    Arreglo pares

--- Clase 1/Ejercicio3.py
+++ Clase 1/Ejercicio3.py
@@ -27,9 +27,9 @@ for i in range(5):
         minimo = numero
     
     if numero % 2 == 0:
-        contador_pares += 1
-        if ingresos_pares == 0:
+        if contador_pares == 0:
             maximo_pares = numero
+        contador_pares += 1
         if maximo_pares < numero:
             maximo_pares = numero
     else:
