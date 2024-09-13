115de468885d5e3876dbb954d7c9be85d454ad63
gh0s7face <eze.cuevass@gmail.com>
2024-09-13 20:56:25 -0300

    ADD: Funciones Recursivas

---
+++ Funciones/recursivas.py
@@ -0,0 +1,21 @@
+# Realizar una función recursiva que calcule la suma de los primeros números naturales:
+# def sumar_naturales(numero)->int:
+#     if numero == 1:
+#         return numero
+#     return numero + sumar_naturales(numero-1)
+# print(sumar_naturales(3))
+# Realizar una función recursiva que calcule la potencia de un número:
+# def calcular_potencia(numero, potencia)->int:
+#     if potencia == 1:
+#         return numero
+#     return numero * (calcular_potencia(numero,potencia-1))
+# print(calcular_potencia(2,4))
+# Realizar una función recursiva que permita realizar la suma de los dígitos de un número:
+# No entendi
+# Realizar una función para calcular el número de Fibonacci de un número ingresado por consola. La función 
+# deberá seguir el siguiente prototipo:
+# def calcular_fibonacci(numero)->int:
+#     if numero <= 1:
+#         return numero
+#     return calcular_fibonacci(numero-1) + calcular_fibonacci(numero-2)
+# print(calcular_fibonacci(int(input("Ingrese numero a calcular fibonacci: "))))
