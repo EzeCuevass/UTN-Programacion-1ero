61079f879480ee4c898d3141c1ea487a8bd7bf6f
gh0s7face <eze.cuevass@gmail.com>
2024-09-16 12:35:22 -0300

    fix: eliminacion de prints en input.py

--- Package_Input/Input.py
+++ Package_Input/Input.py
@@ -10,6 +10,3 @@ def get_float(mensaje="Ingrese un numero flotante: ",mensaje_error="Error, inten
 def get_string(mensaje, mensaje_error="Limite de caracteres alcanzado. Intente otra vez: ", limite=float('+inf'))->str:
     string = validate_string(mensaje,mensaje_error, limite)
     return string
-print(get_int("Ingrese un numero del 1 al 10: ", min=1, max= 10))
-print(get_float("Ingrese un numero del 1 al 10: ", min=1, max= 10))
-print(get_string("Ingrese un string (7 caracteres maximo): ", limite=7))
