ed2b89597b0ad387d34043be7d695ba81acf2ca9
gh0s7face <eze.cuevass@gmail.com>
2024-12-03 01:21:44 -0300

    add: Candy Crush Pygame Avance

--- Candy Crush UTN/juegazo.py
+++ Candy Crush UTN/juegazo.py
@@ -1,75 +1,87 @@
+# Importo funciones, libreries
 import pygame
 from candycrushutn import *
 from maingame import maingame
+from funciones_candycrush import *
+
 pygame.init() # Inicio el juego
 screen = pygame.display.set_mode((800,600)) # Configuro la pantalla
 clock = pygame.time.Clock() 
-running = True
+running = True #Bucle principal
 pygame.display.set_caption("Candy Crush UTN") # Titulo del juego
-
-imagen = pygame.image.load("Candy.png")
-pygame.display.set_icon(imagen)
-imagen = pygame.transform.scale(imagen, (100,100))
-input_box = pygame.Rect(240,300,300,75) # Left, Top, Width, Heigth
-play_box = pygame.Rect(240,400,300,75) # Left, Top, Width, Heigth
+imagen = pygame.image.load("Candy.png") # Imagen de Candy Crush
+pygame.display.set_icon(imagen) # Icono de Candy Crush (al lado del caption)
+imagen = pygame.transform.scale(imagen, (100,100)) # Ajuste de tamaño d la Imagen para usarla en la pantalla principal
+# Rectangulos de input, de jugar y de scoreboard
+input_box = pygame.Rect(240,300,300,75)
+play_box = pygame.Rect(240,400,300,75)
 scoreboard_box = pygame.Rect(20,100, 200, 400)
-color_activo = (232, 232, 232)
-color_inactivo = (191, 191, 191)
-color_input = color_inactivo
-active = False
-texto = ""
-fuente = pygame.font.Font(None, 36)
-fuente_scoreboard =pygame.font.Font(None, 20)
-
+color_activo = (232, 232, 232) # Blanco
+color_inactivo = (191, 191, 191) # Gris
+color_input = color_inactivo # El color default es el inactivo, pero cuando se activa pasa a blanco
+active = False # Bandera para el Input
+texto = "" # Texto del input
+fuente = pygame.font.Font(None, 36) # Fuente
+fuente_scoreboard =pygame.font.Font(None, 20) # Fuente para scoreboard
+# Bucle principal
 while running:
+    # Array contenedor de eventos
     array_eventos = pygame.event.get()
     for event in array_eventos:
+        # Si se presiona la cruz de cierre, se detiene el bucle principal
         if event.type == pygame.QUIT:
             running = False
+        # En caso de que haya un click, entra a un if
         if event.type == pygame.MOUSEBUTTONDOWN:
+            #En el caso de que el click sea en el rectangulo del input, lo activa
             if input_box.collidepoint(event.pos):
-                print("Click en el input")
                 active = True
             else:
-                print("Ckick Afuera del input")
                 active = False
+            # En caso de que el click sea en el rectangulo de "PLAY", usa la funcion de nuevo juego, con texto
+            # y el juego principal (retorna puntos) como parametro
             if play_box.collidepoint(event.pos):
                 print("Jugando al jueguito dou nashe")
                 nuevo_juego(texto,maingame(screen, imagen))
+        # En caso de que se aprete una tecla con el input activo, entra al if
         if event.type == pygame.KEYDOWN and active:
-                if event.key == pygame.K_BACKSPACE:
+                if event.key == pygame.K_BACKSPACE: # Si presiona el DELETE, borra
                     texto = texto[:-1]
-                elif event.key == pygame.K_RETURN:
-                    nuevo_juego(texto,maingame(screen, imagen))
-                else:
-                    texto += event.unicode
-    screen.fill("green")
-    screen.blit(imagen, (340,20))
-    color_input = color_activo if active else color_inactivo
-    pygame.draw.rect(screen, color_input, input_box)
+                elif event.key == pygame.K_RETURN: #Si presiona enter, arranca el nuevo juego
+                    nuevo_juego(texto,maingame(screen, imagen)) 
+                else: # Si presiona una tecla de texto, lo introduce a la cadena 
+                    texto += event.unicode 
+    screen.fill("green") # Fondo verde
+    screen.blit(imagen, (340,20)) # Imagen Candy Crush
+    color_input = color_activo if active else color_inactivo # El color de input, es el activo si la flag esta activa, de lo contrario es el inactivo
+    # Rectangulos, dibujados en la pantalla, color, y los valores de los Rect ya hechos en variables
+    pygame.draw.rect(screen, color_input, input_box) 
     pygame.draw.rect(screen, "purple", play_box)
     pygame.draw.rect(screen, "grey", scoreboard_box)
-    scoreboard_texto = fuente.render("SCOREBOARD", True, "red")
-    play_texto = fuente.render("PLAY",True, "green")
-    texto_renderizado = fuente.render(texto, True, (0, 0, 0))
-    screen.blit(texto_renderizado, (input_box.x + 10, input_box.y + 20))
+    scoreboard_texto = fuente.render("SCOREBOARD", True, "red") # Titulo de Scoreboard
+    play_texto = fuente.render("PLAY",True, "green") # Texto de boton PLAY
+    texto_renderizado = fuente.render(texto, True, (0, 0, 0)) # Texto de input
+    # Bliteo en pantalla de los textos mencionados
+    screen.blit(texto_renderizado, (input_box.x + 10, input_box.y + 20)) 
     screen.blit(play_texto, (play_box.x+120, play_box.y+25))
     screen.blit(scoreboard_texto, (scoreboard_box.x+15, scoreboard_box.y))
+    # Espacio de SCOREBOARD hasta los integrantes del mismo
     espacio = 30
-    lista_scoreboard = obtener_scoreboard()
+    lista_scoreboard = obtener_scoreboard() #Scoreboard
+    # Recorrido de array con FOR
     for persona in lista_scoreboard:
-        cadena_separada = persona.split(",")
-        if len(cadena_separada[0])>17:
+        cadena_separada = persona.split(",") # Separo cada linea del array, con la "," devuelve ["nombre", "puntos"]
+        if len(cadena_separada[0])>17: # En caso de ser muy largo el nombre, lo acorta y le pone puntos suspensivos
             cadena_separada[0] = cadena_separada[0][:17] + "..."
         screen.blit(
             fuente_scoreboard.render(f"{cadena_separada[0]}", True, "green"),
             (scoreboard_box.x, scoreboard_box.y+espacio)
-        )
+        ) # Bliteo el nombre 
         screen.blit(
             fuente_scoreboard.render(f"{cadena_separada[1][:-1]}", True, "green"),
             (scoreboard_box.x+150, scoreboard_box.y+espacio)
-        )
-        
+        ) # Bliteo el puntaje
+        # Sumo 20 de espacio para que no se junten todos
         espacio += 20
     pygame.display.flip()
     clock.tick(30)
--- Candy Crush UTN/maingame.py
+++ Candy Crush UTN/maingame.py
@@ -1,20 +1,24 @@
+# Importaciones de libreria y funciones
 import pygame
 from candycrushutn import *
 from funciones_candycrush import *
+# Funcion
 def maingame(screen, imagen):
-    puntos = 0
+    puntos = 0 # Incializacion de puntos
+    # Seteo de caramelos
     caramelo_rojo  = pygame.image.load("caramelo_rojo.png")
     caramelo_rojo = pygame.transform.scale(caramelo_rojo,(100,100))
     caramelo_azul  = pygame.image.load("caramelo_azul.png")
     caramelo_azul = pygame.transform.scale(caramelo_azul,(100,100))
     caramelo_verde  = pygame.image.load("caramelo_verde.png")
     caramelo_verde = pygame.transform.scale(caramelo_verde,(100,100))
-    fuente = pygame.font.Font(None, 36)
-    box_caramelos = pygame.Rect(50,150,700,400)
-    segundos = 11
-    fin_tiempo = False
-    timer = pygame.USEREVENT
-    pygame.time.set_timer(timer, 1000)
+    fuente = pygame.font.Font(None, 36) # Fuente para contador
+    box_caramelos = pygame.Rect(50,150,700,400) # Rect en el que estan los caramelos
+    segundos = 11 # Tiempo
+    fin_tiempo = False # Bandera
+    timer = pygame.USEREVENT 
+    pygame.time.set_timer(timer, 1000) # Cada 1000 milisegundos, usa el userevent de timer
+    # Incializacion de lista
     lista = [
         {"piezas":[]},
         {"piezas":[]},
@@ -22,70 +26,72 @@ def maingame(screen, imagen):
         {"piezas":[]}
     ]
     mostrar_lista(lista)
+    # Bucle principal, mientras NO se termine el tiempo, ejecuta lo que esta adentro
     while not (fin_tiempo):
+        # Array de eventos
         array_eventos = pygame.event.get()
-        for event in array_eventos:
-            if event.type == pygame.QUIT:
+        for event in array_eventos: # Por cada evento en el array:
+            if event.type == pygame.QUIT: # Si es quit cierra el juego
                 pygame.quit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # Si se presiona el boton del mouse:
+                # Agarro la coordennada x, y la coordenada y para usarlas en la funcion de CC consola
                 coordenada_x = int(round(event.pos[0] / 100,0)-1)
                 coordenada_y = int(round((event.pos[1]-100) / 100,0)-1)
-                if box_caramelos.collidepoint(event.pos):
-                    intento_juego = verificar_eleccion(lista, coordenada_y, coordenada_x)
-                    if intento_juego == 10:
+                if box_caramelos.collidepoint(event.pos): # Si el click esta adentro de la caja que contiene los caramelos: 
+                    intento_juego = verificar_eleccion(lista, coordenada_y, coordenada_x) # Verifico la eleccion con las coordenadas
+                    if intento_juego == 10: # En caso de ser 10:
                         lista = [
                             {"piezas":[]},
                             {"piezas":[]},
                             {"piezas":[]},
                             {"piezas":[]}
-                        ]
+                        ] # Reseteo la lista
                         mostrar_lista(lista)
-                        puntos += 10
-                        pygame.display.flip()
-                    elif intento_juego == -1:
+                        puntos += 10 # Sumo puntos
+                        pygame.display.flip() # Actualizo
+                    elif intento_juego == -1: # En caso de ser -1 (error)
                         lista = [
                             {"piezas":[]},
                             {"piezas":[]},
                             {"piezas":[]},
                             {"piezas":[]}
-                        ]
+                        ] # Reseteo la lista
                         mostrar_lista(lista)
+                        segundos = segundos -1 # Resto un segundo
+                        puntos += -1 # Resto un punto
+                        pygame.display.flip() # Actualizo
+            if event.type == pygame.USEREVENT: # Si hay un userevent
+                if event.type == timer: # Si ese user event corresponde al timer
+                    if fin_tiempo == False: # En caso que no se haya llegado al fin del tiempo, se resta 1 segundo
                         segundos = segundos -1
-                        puntos += -1
-                        pygame.display.flip()
-                    else:
-                        pass
-            if event.type == pygame.USEREVENT:
-                if event.type == timer:
-                    if fin_tiempo == False:
-                        segundos = segundos -1
-                        if segundos <= 0:
-                            fin_tiempo = True
-                            return puntos
-
-        timer_texto = fuente.render(str(segundos), True, "green")
-        screen.fill("purple")
-        screen.blit(imagen, (340,20))
-        screen.blit(timer_texto, (100,20))
-        pygame.draw.rect(screen, (74, 74, 74),box_caramelos) #LEFT, TOP, WIDTH, HEIGHT
-        imprimir_carmelos(lista,screen, caramelo_rojo,caramelo_azul,caramelo_verde)
-        pygame.display.flip()
+                        if segundos <= 0: # Si segundos es menor o igual a 0
+                            fin_tiempo = True # Bajo la bandera, rompe el bucle
+                            return puntos # Retorna puntos
+                        
+        timer_texto = fuente.render(str(segundos), True, "green") # Texto de timer
+        screen.fill("purple") # Fondo violeta
+        screen.blit(imagen, (340,20)) # Imagen candy crush
+        screen.blit(timer_texto, (100,20)) # Bliteo del texto del timer
+        pygame.draw.rect(screen, (74, 74, 74),box_caramelos) # Dibuja la caja de los caramelos en la pantalla
+        imprimir_carmelos(lista,screen, caramelo_rojo,caramelo_azul,caramelo_verde) # Utilizo la funcion de imprimir caramelos
+        pygame.display.flip() # Actualizo
 
 
-def imprimir_carmelos(lista,screen, caramelo_rojo,caramelo_azul,caramelo_verde):
-    coordenadas_fila = 150
-    for objeto in lista:
-        coordenadas_columna = 50
-        for caramelo in objeto["piezas"]:
-            if caramelo == 1:
+def imprimir_carmelos(lista,screen, caramelo_rojo,caramelo_azul,caramelo_verde): # Funcion para imprimir caramelos
+    coordenadas_fila = 150 # Donde va a estar la primera fila
+    for objeto in lista: # Por cada diccionario en la lista:
+        coordenadas_columna = 50 # Donde va a estar la primera columna
+        for caramelo in objeto["piezas"]: # Por cada caramelo en el diccionario:
+            # Dependiendo del valor (1,2,3), blitea un caramelo del color correspondiente, en las coordenadas correspondientes
+            if caramelo == 1: 
                 screen.blit(caramelo_rojo,(coordenadas_columna, coordenadas_fila))
             elif caramelo == 2:
                 screen.blit(caramelo_azul,(coordenadas_columna, coordenadas_fila))
             elif caramelo == 3:
                 screen.blit(caramelo_verde,(coordenadas_columna, coordenadas_fila))
-            coordenadas_columna+=100
-            if coordenadas_columna >= 700:
+            coordenadas_columna+=100 # Con cada caramelo, se suma 100 a la coordenada, para que no esten pegados
+            if coordenadas_columna >= 700: # Cuando llega a 700, resetea
                 coordenadas_columna = 100
-        if coordenadas_fila >= 400:
+        if coordenadas_fila >= 400: # Cuando llega a 400, resetea
             coordenadas_fila = 150
-        coordenadas_fila+=100
+        coordenadas_fila+=100 # Con cada fila, se suma 100 a la coordenada, para que no esten pegadas
